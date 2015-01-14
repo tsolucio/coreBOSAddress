12dc5950db7e4734f8a20263fa60be392da53a55
Joe Bordes <joe@tsolucio.com>
2015-01-14 01:15:06 +0100

    fix license and eliminate unnecessary script load

--- modules/cbAddress/cbAddress.js
+++ modules/cbAddress/cbAddress.js
@@ -2,11 +2,7 @@
  * The contents of this file are subject to the vtiger CRM Public License Version 1.0
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
- * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
- * All Rights Reserved.
  ************************************************************************************/
-document.write("<script type='text/javascript' src='include/js/Inventory.js'></script>");
 
 //function for the popup capture hook
 function cbAddressCapture(recordid,value,target_fieldname) {
